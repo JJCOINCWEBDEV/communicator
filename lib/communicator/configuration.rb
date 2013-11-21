bcb1c0fbea6528be2ad9afe9f7cab5996c9567b8
carier <carierboy@gmail.com>
2013-11-21 19:32:39 +0300

    change init configuration

--- lib/communicator/configuration.rb
+++ lib/communicator/configuration.rb
@@ -3,21 +3,28 @@ module Communicator
     attr_accessor :server, :role, :secret_key, :ssl, :domain, :project, :events, :listeners
 
     def initialize(options = {})
-      options.merge!(configure_with_yaml)
-      options.each {|k,v| self.public_send("#{k}=", v) if self.respond_to?("#{k}=") }
+      set_options(options)
+    end
+
+    def configure_with= (yaml_file_path)
+      set_options(configure_with_yaml(yaml_file_path))
     end
 
     private
 
-    def configure_with_yaml
+    def set_options(options = {})
+      options.each {|k,v| self.public_send("#{k}=", v) if self.respond_to?("#{k}=") }
+    end
+
+    def configure_with_yaml(yaml_file_path)
       begin
-        config = YAML.load_file(File.join(Rails.root, "config/communications.yml"))
+        config = YAML.load_file(File.join(Rails.root, yaml_file_path))
       rescue Errno::ENOENT
         Rails.logger.error("YAML configuration file couldn't be found. Using defaults."); return
       rescue Psych::SyntaxError
         Rails.logger.error("YAML configuration file contains invalid syntax. Using defaults."); return
       end
-      config
+      config || {}
     end
   end
 end
